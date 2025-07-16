d3d19e78e1aaced949d73c1be1a60d297c69e96d
Ricardominom <110798786+Ricardominom@users.noreply.github.com>
2025-07-16 13:49:57 -0600

    Eliminar animación shimmer de barras de progreso

--- src/pages/TerritorialPage.tsx
+++ src/pages/TerritorialPage.tsx
@@ -190,7 +190,6 @@ export default function TerritorialPage() {
                           }`}
                           style={{ width: `${region.progress}%` }}
                         >
-                          <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/20 to-transparent animate-shimmer"></div>
                         </div>
                       </div>
                     </div>
